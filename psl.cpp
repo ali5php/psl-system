0c7fae14aa5ac4a4d222a2d56fa13a135a02e94e
ali5php <ali-muhammad473@hotmail.com>
2025-01-24 21:44:02 +0500

    update

--- psl.cpp
+++ psl.cpp
@@ -1,38 +1,169 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    string name[6] = {"Lahore Qalanders", "Karachi Kings",
-                      "Islamabad United", "Multan Sultan",
-                      "Peshawar Zalmi",   "Quetta Galadiaters"};
-    string home[6] = {"Lahore", "Karachi",  "Islamabad",
-                      "Multan", "Peshawar", "Quetta"};
-    string ground[6] = {"Gaddafi Stadium",    "National Stadium",
-                        "Rawalpindi Stadium", "Multan Cricket Stadium",
-                        "Arbab Niaz Satdium", "Bugti Stadium"};
-    string namep[30] = {
-        "player1",  "player2",  "player3",  "player4",  "player5",  "player6",
-        "player7",  "player8",  "player9",  "player10", "player11", "player12",
-        "player13", "player14", "player15", "player16", "player17", "player18",
-        "player19", "player20", "player21", "player22", "player23", "player24",
-        "player25", "player26", "player27", "player28", "player29", "player30"};
-    int age[30] = {25, 26, 27, 28, 29, 30, 24, 23, 31, 32, 28, 29, 30, 26, 27,
-                   25, -3, 34, 32, 31, 24, 26, 28, 30, 27, 29, 25, 32, 31, 26};
-    string role[30] = {"Batsman", "Bowler", "All-rounder", "Wicketkeeper", "Bowler",
-                "Batsman", "Bowler", "All-rounder", "Batsman",      "Bowler",
-                "Batsman", "Bowler", "All-rounder", "Wicketkeeper", "Bowler",
-                "Batsman", "Bowler", "All-rounder", "Batsman",      "Bowler",
-                "Batsman", "Bowler", "All-rounder", "Wicketkeeper", "Bowler",
-                "Batsman", "Bowler", "All-rounder", "Batsman",      "Bowler"};
-    for (int i = 0; i < 6; i++)
+    int choice, max = 0, mindex = 0;
+    string name[6] = {"Karachi Kings",     "Lahore Qalandars",
+                      "Peshawar Zalmi",    "Islamabad United",
+                      "Quetta Gladiators", "Multan Sultans"},
+
+           hgro[6] = {"National Stadium",   "Gaddafi Stadium",
+                      "Arbab Niaz Stadium", "Rawalpindi Cricket Stadium",
+                      "Bugti Stadium",      "Multan Cricket Stadium"},
+
+           hcity[6] = {"Karachi",   "Lahore", "Peshawar",
+                       "Islamabad", "Quetta", "Multan Sultans"},
+
+           pname[30] =
+               {"Babar Azam",         "Mohammad Rizwan", "Shaheen Shah Afridi",
+                "Shadab Khan",        "Fakhar Zaman",    "Haris Rauf",
+                "Imam-ul-Haq",        "Naseem Shah",     "Hasan Ali",
+                "Iftikhar Ahmed",     "Mohammad Nawaz",  "Usama Mir",
+                "Abrar Ahmed",        "Saim Ayub",       "Agha Salman",
+                "Mohammad Wasim Jr.", "Shan Masood",     "Sarfaraz Ahmed",
+                "Mohammad Haris",     "Khushdil Shah",   "Mohammad Hasnain",
+                "Zaman Khan",         "Tayyab Tahir",    "Ihsanullah",
+                "Kamran Ghulam",      "Mohammad Ali",    "Mir Hamza",
+                "Faheem Ashraf",      "Saud Shakeel",    "Abdullah Shafique"},
+
+           prole[30] = {"Batsman",      "Wicketkeeper", "Bowler",
+                        "All-rounder",  "Batsman",      "Bowler",
+                        "Batsman",      "Bowler",       "Bowler",
+                        "All-rounder",  "All-rounder",  "Bowler",
+                        "Bowler",       "Batsman",      "Batsman",
+                        "All-rounder",  "Batsman",      "Wicketkeeper",
+                        "Wicketkeeper", "Batsman",      "Bowler",
+                        "Bowler",       "Batsman",      "Bowler",
+                        "Batsman",      "Bowler",       "Bowler",
+                        "Bowler",       "Batsman",      "Batsman"}
+
+    ,
+           plq[30] = {" "}, pkk[30] = {" "}, ppz[30] = {" "}, piu[30] = {" "},
+           pqg[30] = {" "}, pms[30] = {" "};
+
+    int price[6] = {-1};
+    bool flag = true, inflag = true;
+    while (flag)
     {
-        cout << name[i] << " ";
+        cout << " Welcome to the PSL Management System:" << endl;
+        cout << " 1. View Teams and Home Grounds:" << endl;
+        cout << " 2. Draft Players for Teams" << endl;
+        cout << " 3. View Match Schedule " << endl;
+        cout << " 4. Enter Match Results " << endl;
+        cout << " 5. Display Points Table" << endl;
+        cout << " 6. Search for Teams or Players" << endl;
+        cout << " 7. Exit " << endl;
+        cout << "Enter your choice:";
+        cin >> choice;
+        if (choice == 1)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                cout << "Team Name:- " << name[i] << " City:- " << hcity[i]
+                     << " Ground:- " << hgro[i] << "\n";
+            }
+            flag = true;
+        }
+        else if (choice == 2)
+        {
+            cout << "Bidding for The Players" << endl;
+            for (int i = 0; i < 3; i++)
+            {
+                cout << "Player:- ";
+                cout << pname[i];
+                cout << endl;
+                for (int j = 0; j < 6; j++)
+                {
+                    cout << name[j] << endl;
+                    cout << "Team enter yout current Bid:- \n";
+                    cin >> price[j];
+                    while (inflag == true)
+                    {
+                        for (int l = 0; l < 6; l++)
+                        {
+                            if (price[j] == price[l] && l != j)
+                            {
+                                cout
+                                    << "Team enter yout current Bid(Bid amount "
+                                       "is already present, bid higher):- \n";
+                                cin >> price[j];
+                                break;
+                            }
+                            else
+                            {
+                                inflag = false;
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < 6; j++)
+                {
+                    inflag = true;
+                    mindex = -1;
+                    max = 0;
+                    for (int k = 0; k < 6; k++)
+                    {
+                        if (price[k] > max)
+                        {
+                            max = price[k];
+                            mindex = k;
+                        }
+                    }
+                    cout << mindex << " ";
+                    if (mindex == 0)
+                    {
+                        pkk[i] = pname[i];
+                    }
+                    if (mindex == 1)
+                    {
+                        plq[i] = pname[i];
+                    }
+                    if (mindex == 2)
+                    {
+                        ppz[i] = pname[i];
+                    }
+                    if (mindex == 3)
+                    {
+                        piu[i] = pname[i];
+                    }
+                    if (mindex == 4)
+                    {
+                        pqg[i] = pname[i];
+                    }
+                    if (mindex == 5)
+                    {
+                        pms[i] = pname[i];
+                    }
+                }
+                int price[6] = {-1};
+            }
+            flag = true;
+        }
+        else if (choice == 3)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                cout << "LAHORE QALANDERS:- " << plq[i] << "\n\n";
+                cout << "Karachi Kings:- " << pkk[i] << "\n\n";
+                cout << "Peshawar Zalmi:- " << ppz[i] << "\n\n";
+                cout << "Islamabad united:- " << piu[i] << "\n\n";
+                cout << "Quetta Gladiators:- " << pqg[i] << "\n\n";
+                cout << "Multan Sultans:- " << pms[i] << "\n\n";
+            }
+            flag = true;
+        }
+        else if (choice == 4)
+        {
+            flag = true;
+        }
+        else if (choice == 5)
+        {
+            flag = true;
+        }
+        else if (choice == 6)
+        {
+            flag = false;
+        }
     }
-    cout << '\n';
-    for (int i = 0; i < 6; i++)
-    {
-        cout << home[i] << " ";
-    }
+    return 0;
 }
